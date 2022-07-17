706c6435eb978582acc5a290678a3c3e979679a2
Chloe Lee <chloelee00516@gmail.com>
2022-07-17 23:37:14 +0900

    added bootstrap
    bootstrap-accordion

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 // import Swup from 'swup';
-const swup = new Swup(); // only this line when included with script tag
+// const swup = new Swup(); // only this line when included with script tag
+
+var swup = new Swup({
+    animationSelector: '[class*="swup-transition-"]'
+  });
+
+
+
+
+// Rotate icon - Bootstrap
+let rotateBtn = document.getElementsByClassName("rotate-icon")
+$("#myButton").click(function(e) {
+    $(this).addClass("rotate-animation");
+});
